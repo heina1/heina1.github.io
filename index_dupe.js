eb82fe1e54d056e7c7aa38cac8f4e81d5b7ce317
heina <163164108+heina1@users.noreply.github.com>
2024-03-28 12:35:37 -0400

    Delete index_dupe.js

--- index_dupe.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('settings-btn').addEventListener('click', function() {
-    var iframe = document.getElementById('settings-iframe');
-    if (iframe.style.display === 'none') {
-        iframe.style.display = 'block';
-    } else {
-        iframe.style.display = 'none';
-    }
-});
