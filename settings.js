2af7cde2681c0d24a506072be8df68a9a8a5e5b4
heina <163164108+heina1@users.noreply.github.com>
2024-03-28 12:35:04 -0400

    Delete settings.js

--- settings.js
+++
@@ -1,9 +0,0 @@
-function toggleSettings() {
-  var settingsFrame = document.getElementById("settingsFrame");
-  // Toggle visibility of the settings frame
-  if (settingsFrame.style.display === "none") {
-    settingsFrame.style.display = "block";
-  } else {
-    settingsFrame.style.display = "none";
-  }
-}
